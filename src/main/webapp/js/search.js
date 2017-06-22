ac7dec4fd69e53294bd9099b927b4c423d794884
hojha <harshvardhanojha@mindagroup.com>
2017-06-22 09:44:47 +0530

    removed autocomplete to fix page flicker

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -6,15 +6,15 @@ function selectDocument(docId) {
     if (docId == lastDocId) {
         return;
     }
-    
+
     $("#row-" + docId).addClass("result-list-row-selected");
     $("#doc-" + docId).show();
-    
+
     if (lastDocId != null) {
         $("#row-" + lastDocId).removeClass("result-list-row-selected");
         $("#doc-" + lastDocId).hide();
     }
-    
+
     lastDocId = docId;
 }
 
@@ -25,18 +25,18 @@ function initPage(docId) {
 
 function newTagEnter(docId, e) {
     var charCode;
-    
+
     if (e && e.which) {
         charCode = e.which;
-    } else if(window.event) {
+    } else if (window.event) {
         e = window.event;
         charCode = e.keyCode;
     }
 
-    if(charCode != 13) {
+    if (charCode != 13) {
         return;
     }
-    
+
     newTag(docId);
 }
 
@@ -45,62 +45,62 @@ function newTag(docId) {
     if (tag == null || tag.length == 0) {
         return;
     }
-    
+
     $.ajax({
-      type: 'POST',
-      url: 'tag.html',
-      data: { action: 'newtag', docid: docId, tag: tag},
-      success:function(data){
-        if (data != 'SUCCESS') {
-            return;
+        type: 'POST',
+        url: 'tag.html',
+        data: {action: 'newtag', docid: docId, tag: tag},
+        success: function (data) {
+            if (data != 'SUCCESS') {
+                return;
+            }
+
+            displayTag(docId, tag);
+
+            $("#tag-doc-" + docId).hide();
+            $("#tag-doc-field-" + docId).val('');
+        },
+        error: function () {
+            alert("Technical error, try that again in a few moments!");
         }
-        
-        displayTag(docId, tag);
-        
-        $("#tag-doc-" + docId).hide();
-        $("#tag-doc-field-" + docId).val('');
-      },
-      error:function(){
-        alert("Technical error, try that again in a few moments!");
-      }
     });
 }
 
 function displayTag(docId, tag) {
     addCaseTag(tag);
-    
+
     if (documentsMap[docId][tag] != null) {
         return;
     } else {
         documentsMap[docId][tag] = 1;
     }
-    
+
     var docIdParam = '"' + docId + '"';
     var tagParam = '"' + tag + '"';
     $("#tags-table-" + docId).append("<tr class='document-tags-row'>" +
-                              "<td><div class='document-tags-tag'>" + tag + "</div></td>" +
-                              "<td><a href='#' onclick='deleteTag(" + docIdParam + ", this, " + tagParam + ")'><img src='images/delete.gif'/></a></td>" +
-                           "</tr>");
+        "<td><div class='document-tags-tag'>" + tag + "</div></td>" +
+        "<td><a href='#' onclick='deleteTag(" + docIdParam + ", this, " + tagParam + ")'><img src='images/delete.gif'/></a></td>" +
+        "</tr>");
     var total = parseInt($("#tags-total-" + docId).html()) + 1;
     $("#tags-total-" + docId).html(total);
 }
 
 function deleteTag(docId, el, tag) {
     $.ajax({
-      type: 'POST',
-      url: 'tag.html',
-      data: { action: 'deletetag', docid: docId, tag: tag},
-      success:function(data){
-        $(el).parent().parent().remove();
-        var total = parseInt($("#tags-total-" + docId).html()) - 1;
-        $("#tags-total-" + docId).html(total);
-        if (total == 0) {
-            $("#tags-box-" + docId).hide();
+        type: 'POST',
+        url: 'tag.html',
+        data: {action: 'deletetag', docid: docId, tag: tag},
+        success: function (data) {
+            $(el).parent().parent().remove();
+            var total = parseInt($("#tags-total-" + docId).html()) - 1;
+            $("#tags-total-" + docId).html(total);
+            if (total == 0) {
+                $("#tags-box-" + docId).hide();
+            }
+        },
+        error: function () {
+            alert("Technical error, try that again in a few moments!");
         }
-      },
-      error:function(){
-        alert("Technical error, try that again in a few moments!");
-      }
     });
 }
 
@@ -108,129 +108,129 @@ function search() {
     var queryStr = $("#search-query").val();
 
     $.ajax({
-      type: 'POST',
-      url: 'dosearch.html',
-      data: { action: 'search', query: queryStr},
-      success:function(data){
-        lastDocId = null;
-        
-        $("#result-ajax").html(data);
-        
-        var solrId = $("#solrid").val();
-        if (solrId != null) {
-            initPage(solrId);
+        type: 'POST',
+        url: 'dosearch.html',
+        data: {action: 'search', query: queryStr},
+        success: function (data) {
+            lastDocId = null;
+
+            $("#result-ajax").html(data);
+
+            var solrId = $("#solrid").val();
+            if (solrId != null) {
+                initPage(solrId);
+            }
+
+            $("#search-query").val('');
+        },
+        error: function () {
+            alert("Technical error, try that again in a few moments!");
         }
-        
-        $("#search-query").val('');
-      },
-      error:function(){
-        alert("Technical error, try that again in a few moments!");
-      }
     });
 }
 
 function addTagToSearch(tag) {
     $.ajax({
-      type: 'POST',
-      url: 'dosearch.html',
-      data: { action: 'tagsearch', tag: tag},
-      success:function(data){
-        lastDocId = null;
-        
-        $("#result-ajax").html(data);
-        
-        var solrId = $("#solrid").val();
-        if (solrId != null) {
-            initPage(solrId);
+        type: 'POST',
+        url: 'dosearch.html',
+        data: {action: 'tagsearch', tag: tag},
+        success: function (data) {
+            lastDocId = null;
+
+            $("#result-ajax").html(data);
+
+            var solrId = $("#solrid").val();
+            if (solrId != null) {
+                initPage(solrId);
+            }
+        },
+        error: function () {
+            alert("Technical error, try that again in a few moments!");
         }
-      },
-      error:function(){
-        alert("Technical error, try that again in a few moments!");
-      }
     });
 }
 
 function changePage(page) {
     $.ajax({
-      type: 'POST',
-      url: 'dosearch.html',
-      data: { action: 'changepage', page: page},
-      success:function(data){
-        lastDocId = null;
-      
-        $("#result-ajax").html(data);
-        
-        var solrId = $("#solrid").val();
-        if (solrId != null) {
-            initPage(solrId);
+        type: 'POST',
+        url: 'dosearch.html',
+        data: {action: 'changepage', page: page},
+        success: function (data) {
+            lastDocId = null;
+
+            $("#result-ajax").html(data);
+
+            var solrId = $("#solrid").val();
+            if (solrId != null) {
+                initPage(solrId);
+            }
+        },
+        error: function () {
+            alert("Technical error, try that again in a few moments!");
         }
-      },
-      error:function(){
-        alert("Technical error, try that again in a few moments!");
-      }
     });
 }
 
 function removeSearch(id) {
     $.ajax({
-      type: 'POST',
-      url: 'dosearch.html',
-      data: { action: 'remove', id: id},
-      success:function(data){
-        lastDocId = null;
-      
-        $("#result-ajax").html(data);
-        
-        var solrId = $("#solrid").val();
-        if (solrId != null) {
-            initPage(solrId);
+        type: 'POST',
+        url: 'dosearch.html',
+        data: {action: 'remove', id: id},
+        success: function (data) {
+            lastDocId = null;
+
+            $("#result-ajax").html(data);
+
+            var solrId = $("#solrid").val();
+            if (solrId != null) {
+                initPage(solrId);
+            }
+        },
+        error: function () {
+            alert("Technical error, try that again in a few moments!");
         }
-      },
-      error:function(){
-        alert("Technical error, try that again in a few moments!");
-      }
     });
 }
 
 function removeAllSearch() {
     $.ajax({
-      type: 'POST',
-      url: 'dosearch.html',
-      data: { action: 'removeall'},
-      success:function(data){
-        lastDocId = null;
-      
-        $("#result-ajax").html(data);
-        
-        var solrId = $("#solrid").val();
-        if (solrId != null) {
-            initPage(solrId);
+        type: 'POST',
+        url: 'dosearch.html',
+        data: {action: 'removeall'},
+        success: function (data) {
+            lastDocId = null;
+
+            $("#result-ajax").html(data);
+
+            var solrId = $("#solrid").val();
+            if (solrId != null) {
+                initPage(solrId);
+            }
+        },
+        error: function () {
+            alert("Technical error, try that again in a few moments!");
         }
-      },
-      error:function(){
-        alert("Technical error, try that again in a few moments!");
-      }
     });
 }
 
 function initTags() {
-   $(".document-tags-table").hide();
-   $(".document-tags-label").click(function() {
-       $(this).next(".document-tags-table").slideToggle(500);
-   });
-   
-   $(".solrid").each(function(index) {
-       var docId = $(this).val();
-       documentsMap[docId] = new Object();
-       $(".doc-tag-" + docId).each(function(index) {
-           var tag = $(this).val();
-           documentsMap[docId][tag] = 1;
-       });
-   });
-   
-   $(".tag-doc-field-cl").autocomplete({source : "tagauto.html"});
-   $("#tag-all-text").autocomplete({source : "tagauto.html"});
-   $("#tag-page-text").autocomplete({source : "tagauto.html"});
+    $(".document-tags-table").hide();
+    $(".document-tags-label").click(function () {
+        $(this).next(".document-tags-table").slideToggle(500);
+    });
+
+    $(".solrid").each(function (index) {
+        var docId = $(this).val();
+        documentsMap[docId] = new Object();
+        $(".doc-tag-" + docId).each(function (index) {
+            var tag = $(this).val();
+            documentsMap[docId][tag] = 1;
+        });
+    });
+
+    //$(".tag-doc-field-cl").autocomplete({source : "tagauto.html"});
+    $("#tag-all-text").autocomplete({source: "tagauto.html"});
+    $("#tag-page-text").autocomplete({source: "tagauto.html"});
 }
 
 function tagAllBox() {
@@ -245,18 +245,18 @@ function tagPageBox() {
 
 function newAllTagEnter(callFunc, e) {
     var charCode;
-    
+
     if (e && e.which) {
         charCode = e.which;
-    } else if(window.event) {
+    } else if (window.event) {
         e = window.event;
         charCode = e.keyCode;
     }
 
-    if(charCode != 13) {
+    if (charCode != 13) {
         return;
     }
-    
+
     callFunc();
 }
 
@@ -273,26 +273,26 @@ function tagDocuments(textId, boxId, action) {
     if (tag == null || tag.length == 0) {
         return;
     }
-    
+
     $.ajax({
-      type: 'POST',
-      url: 'tag.html',
-      data: { action: action, tag: tag},
-      success:function(data) {
-        if (data != 'SUCCESS') {
-            return;
+        type: 'POST',
+        url: 'tag.html',
+        data: {action: action, tag: tag},
+        success: function (data) {
+            if (data != 'SUCCESS') {
+                return;
+            }
+
+            for (var docId in documentsMap) {
+                displayTag(docId, tag);
+            }
+
+            $("#" + boxId).hide();
+            $("#" + textId).val('');
+        },
+        error: function () {
+            alert("Technical error, try that again in a few moments!");
         }
-        
-        for (var docId in documentsMap) {
-            displayTag(docId, tag);
-        }
-        
-        $("#" + boxId).hide();
-        $("#" + textId).val('');
-      },
-      error:function(){
-        alert("Technical error, try that again in a few moments!");
-      }
     });
 }
 
@@ -307,41 +307,41 @@ function appendCaseTag(tag) {
     $(".case-tags-box-body").append("<div id='" + tag + "' class='case-tags-box-row' onclick='addTagToSearch(\"" + tag + "\")'>" + tag + "</div>");
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("body").bind({
-        ajaxStart: function() { 
-            $(this).addClass("loading"); 
+        ajaxStart: function () {
+            $(this).addClass("loading");
         },
-        ajaxStop: function() { 
-            $(this).removeClass("loading"); 
-        }    
+        ajaxStop: function () {
+            $(this).removeClass("loading");
+        }
     });
-    
-    $('#search-query').keypress(function(e) {
+
+    $('#search-query').keypress(function (e) {
         if (e.keyCode == 13) {
             search();
         }
     });
-    
+
     for (var t in allTags) {
         appendCaseTag(t);
     }
-    
+
     $("body").on("click", ".html-preview", function () {
-    	var docId = $(this).attr("data");
-    	var uId = $(this).attr("uid");
-    	
-    	$.ajax({
-	      type: 'GET',
-	      url: 'filedownload.html',
-	      data: { action: 'exportHtml', docPath : docId, uniqueId : uId},
-	      success:function(data) {
-	        $("#html_preview_modal_content").html(data);
-	        $('#html_preview_modal').modal('show');
-	      },
-	      error:function(){
-	        alert("Technical error, try that again in a few moments!");
-	      }
-    	});
+        var docId = $(this).attr("data");
+        var uId = $(this).attr("uid");
+
+        $.ajax({
+            type: 'GET',
+            url: 'filedownload.html',
+            data: {action: 'exportHtml', docPath: docId, uniqueId: uId},
+            success: function (data) {
+                $("#html_preview_modal_content").html(data);
+                $('#html_preview_modal').modal('show');
+            },
+            error: function () {
+                alert("Technical error, try that again in a few moments!");
+            }
+        });
     });
 });
