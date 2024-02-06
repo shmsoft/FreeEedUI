1902dd40da6fb728c6cf35133bbc35f30bbf3141
Marlon <masj1a@gmail.com>
2024-02-06 01:43:44 +0000

    Add support to multi cases

--- src/main/webapp/js/freeedai.js
+++ src/main/webapp/js/freeedai.js
@@ -6,41 +6,89 @@ function onSubmit(event)
     container.innerHTML += newHTMLContent;
     $('#send_question').prop('disabled', true);
     $('.question_input').prop('disabled', true);
-    $.ajax({
-        type: 'GET',
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        },
-        url: $("#aiApiUrl").val() + '/question_case/',
-        data: { case_id: 'freeeed_' + $("#aiApiKey").val() + '_' + $('.your-case-select').val(), question: $(".question_input").val()},
-        success:function(data) {
-            var sourcesHtml = '';
-            var caseId = $("#case_Select").val();
-            if(data.sources && data.sources.length > 0)
-            {
-                sourcesHtml = '<small class="source">';
-                for (let index = 0; index < data.sources.length; index++) {
-                    var source = data.sources[index];
-                    source = '<a target="_blank" href="' +  getCurrentUrl() + '/search.html?caseid=' + caseId + '&query=UPI:' + source + '">' + source + '</a>'
-                    sourcesHtml = sourcesHtml +  source + ' | ';
-                }
-                sourcesHtml = sourcesHtml + '</small>';
-            }
 
-            newHTMLContent = '<div class="answer">' + data.answer + sourcesHtml + '</div>';
-            container.innerHTML += newHTMLContent;
-            $(".question_input").val('');
-            $('#send_question').prop('disabled', false);
-            $('.question_input').prop('disabled', false);
-        },
-        error: function(xhr, status, error) {
-            console.error(xhr.responseText);
-            alert("Technical error, try that again in a few moments!");
-            $('#send_question').prop('disabled', false);
-            $('.question_input').prop('disabled', false);
-            $(".question_input").val('');
-        }
-    });
+    var allCases = $("#allCasesCheckbox").prop("checked");
+    var url = "";
+    var data = {};
+    if(allCases)
+    {
+        url = $("#aiApiUrl").val() + '/question_mult_cases/';
+        data = {
+            question: $(".question_input").val()
+        };
+        var cases = $(".your-case-select").find('option');
+        cases.each(function() {
+            data["case_ids"] = 'freeeed_' + $("#aiApiKey").val() + '_' + $(this).val();
+        });
+    }
+    else {
+        url = $("#aiApiUrl").val() + '/question_case/';
+        data = {
+            case_id: 'freeeed_' + $("#aiApiKey").val() + '_' + $('.your-case-select').val(),
+            question: $(".question_input").val()
+        };
+    }
+        $.ajax({
+            type: 'GET',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            url: url,
+            data: data,
+            success:function(data) {
+                var sourcesHtml = '';
+
+                if(allCases)
+                {
+                    data.each(function() {
+                        var caseId = this.case_id;
+                        if(this.sources && this.sources.length > 0)
+                        {
+                            sourcesHtml = '<small class="source">';
+                            for (let index = 0; index < this.sources.length; index++) {
+                                var source = this.sources[index];
+                                source = '<a target="_blank" href="' +  getCurrentUrl() + '/search.html?caseid=' + caseId + '&query=UPI:' + source + '">' + source + '</a>'
+                                sourcesHtml = sourcesHtml +  source + ' | ';
+                            }
+                            sourcesHtml = sourcesHtml + '</small>';
+                        }
+                        newHTMLContent = '<div class="answer">' + this.answer + sourcesHtml + '</div>';
+                        container.innerHTML += newHTMLContent;
+                     });
+                }
+                else
+                {
+                    var caseId = $("#case_Select").val();
+                    if(data.sources && data.sources.length > 0)
+                    {
+                        sourcesHtml = '<small class="source">';
+                        for (let index = 0; index < data.sources.length; index++) {
+                            var source = data.sources[index];
+                            source = '<a target="_blank" href="' +  getCurrentUrl() + '/search.html?caseid=' + caseId + '&query=UPI:' + source + '">' + source + '</a>'
+                            sourcesHtml = sourcesHtml +  source + ' | ';
+                        }
+                        sourcesHtml = sourcesHtml + '</small>';
+                    }
+
+                    newHTMLContent = '<div class="answer">' + data.answer + sourcesHtml + '</div>';
+                    container.innerHTML += newHTMLContent;
+                }
+
+
+
+                $(".question_input").val('');
+                $('#send_question').prop('disabled', false);
+                $('.question_input').prop('disabled', false);
+            },
+            error: function(xhr, status, error) {
+                console.error(xhr.responseText);
+                alert("Technical error, try that again in a few moments!");
+                $('#send_question').prop('disabled', false);
+                $('.question_input').prop('disabled', false);
+                $(".question_input").val('');
+            }
+        });
+
 }
 
 function getCurrentUrl()
@@ -68,5 +116,11 @@ function sendQuestion(event)
 
 $(document).ready(function() {
 
-
+    $("#allCasesCheckbox").change(function() {
+        if(this.checked) {
+            $("#case_Select").prop('disabled', true);
+        } else {
+            $("#case_Select").prop('disabled', false);
+        }
+    });
 });
